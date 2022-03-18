d0b3da308b0f7eb51cbf9b5bcfd642ad9dfdf175
Muhammad Yusuf Adi Purwanto <muhammadyusufadip.1999@gmail.com>
2022-03-18 20:32:55 +0700

    update axios

--- src/app/libs/server/axios.ts
+++ src/app/libs/server/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import errorHandler from "./errorHandler";
 
+console.log(process.env.REACT_APP_API_URL);
+
 const instance = axios.create({
-  baseURL: "",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 instance.interceptors.response.use((response) => response, errorHandler);
